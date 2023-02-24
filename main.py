53793fee9124091f2c1955ec2c332b326ba19f33
olegrix2222 <125788873+olegrix2222@users.noreply.github.com>
2023-02-24 14:42:28 +0000

    cat

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ class Person:
   weight = 15
   height = 1
   age = 4
+  eats = 3
 
 Cat = Person()
 
 print(Cat.age)
 Cat.age +=  1
 print(Cat.age)
+
+print(Cat.eats)
+Cat.eats +=  1
+print(Cat.eats)
